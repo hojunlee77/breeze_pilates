1c82396c4d671722a96d98964c0aca632aff4a46
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-05-25 14:17:17 +0900

    google map, footer info

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const building = document.getElementById("map");
+// const building = document.getElementById("map");
 
-const map = new naver.maps.Map(building, {
-  center: new naver.maps.LatLng(37.3595704, 127.105399),
-  zoom: 10,
-});
+// const map = new naver.maps.Map(building, {
+//   center: new naver.maps.LatLng(37.3595704, 127.105399),
+//   zoom: 9,
+// });
